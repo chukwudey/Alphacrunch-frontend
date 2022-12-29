70d46d1cfbee3820f0b293230a3e14b35c1cd653
Vastro-lorde <omatsolaseund@gmail.com>
2022-12-29 12:01:03 +0100

    added Aria label to emoji

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
 import {AiOutlineEye, AiOutlineEyeInvisible} from 'react-icons/ai';
 import signin_bg from '../assets/signin_bg.png';
 
@@ -10,17 +11,17 @@ const LoginPage = () => {
     })
   return (
     <div className=' flex justify-start items-center'>
-      <div className=' h-screen w-5/12'>
+      <div className=' hidden md:block h-screen w-5/12'>
         <img src={signin_bg} alt="futuristic computer desk with holographic bitcoin" className=' h-full'/>
       </div>
-      <div className=' w-7/12'>
+      <div className=' w-full md:w-7/12'>
         <form className=' mx-auto p-8 block'>
-            <h1 className=' text-5xl text-yellow-500 '>Welcome Chief! <span role='img'>&#128075;</span></h1>
-            <p>Signin to continue</p>
-            <div className=' border border-solid border-gray-500 rounded-lg p-0 overflow-hidden focus:outline-none w-2/3 my-4'>
+            <h1 className=' text-3xl md:text-5xl text-yellow-500 text-center mb-4 '>Welcome Chief! <span role='img' aria-label="Waving Hand" className=' border-none'>&#128075;</span></h1>
+            <p className=' text-center text-xl mb-8'>Signin to continue</p>
+            <div className=' border border-solid border-gray-500 rounded-lg p-0 overflow-hidden focus:outline-none mx-auto w-2/3 my-4'>
                 <input type="email" className=' pl-6 pr-1 py-2 w-full' name="email" value={formData.email} onChange={(e)=> setFormData({email: e.currentTarget.value})} placeholder='Enter Your Email'/>
             </div>
-            <div className=' flex justify-between items-center border border-solid border-gray-500 rounded-lg p-0 overflow-hidden focus:outline-none w-2/3'>
+            <div className=' flex justify-between items-center border border-solid border-gray-500 rounded-lg p-0 overflow-hidden mx-auto focus:outline-none w-2/3'>
                 <input type={show? 'text': 'password'} 
                     name="password"
                     className=' pl-6 pr-2 py-2 w-full'
@@ -35,6 +36,7 @@ const LoginPage = () => {
                         <AiOutlineEye onClick={()=> setShow(!show)}/>
                     </div> }
             </div>
+            <Link></Link>
         </form>
         
       </div>
